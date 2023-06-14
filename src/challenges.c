1809e710dc30fb482e3d89920243af64697ac857
Aj <aj@ajae.uk>
2023-06-14 13:54:37 +0100

    Completed Challenge 2 - Enter a name

--- src/challenges.c
+++ src/challenges.c
@@ -8,8 +8,18 @@ static void Challenge_1()
     puts("Kara-tea");
 }
 
+static void Challenge_2()
+{
+    char name[15];
+    puts("Please enter your name");
+
+    scanf("%s", name);
+
+    printf("\nHello, %s\n", name);
+}
+
 int main(int argc, char const *argv[])
 {
-    Challenge_1();
+    Challenge_2();
     return 0;
 }
